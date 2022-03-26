8fa438cd1da6552fff7dfd0fe20fb12573ba7488
Ruslan A.Bityutskih <r.bituckih@gmail.com>
2022-03-26 19:48:15 +0300

    003 - Coffe mashine add  amount, drink, price, test

--- src/main/java/org/example/coffemashine/Runner.java
+++ src/main/java/org/example/coffemashine/Runner.java
@@ -1,7 +1,49 @@
 package org.example.coffemashine;
-
+import java.awt.image.BufferedImage;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class Runner {
-    public static void main(String[] args) {
-        System.out.println("AUTOMATED COFFEE-MASHINE");
+    public static void main (String[] args) {
+        System.out.println("Кофе-машина");
+
+        //TODO: read int from console
+        int moneyAmount = 20;
+
+        System.out.println("Вы внесли " + moneyAmount + " руб.");
+
+        int[] drinkPrices = {150, 80, 20, 50};
+        String[] drinkNames = {"капучино", "эспрессо", "воду", "молоко"};
+
+        boolean canBuySomething = false;
+
+        for(int i = 0; i < drinkPrices.length; i = i + 1) {
+            if(moneyAmount >= drinkPrices[i]) {
+                System.out.println("Вы можете купить " +
+                        drinkNames[i]);
+                canBuySomething = true;
+            }
+        }
+
+        if(!canBuySomething) {
+            System.out.println("Недостаточно средств :( Изучайте Java и зарабатывайте много!))");
+        }
+
+        //TODO:
+        //* SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        //Date now = new Date();
+        //System.out.println(formatter.format(now));*/
+
+        //TODO: get screenshot to BufferedImage
+        //BufferedImage image = null;
+        //assert image != null;
+        //System.out.println(image.getWidth() + "x" +
+        //        image.getHeight());
+
+        //TODO:
+        //  upload screenshot(s) of the code
+        //  and console output and send to Telegram-chat
+        //  with #ilovejava
     }
 }
